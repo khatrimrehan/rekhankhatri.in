3dcbd67e44cdd31aed54b4ba6efbb1f85660bbee
RK <khatrimrehan@gmail.com>
2026-03-10 01:24:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+var csr = document.querySelector("#cursor")
+
+
+document.addEventListener("mousemove", function (dets) {
+
+    setTimeout(function () {
+
+        csr.style.left = dets.x + 0 + "px"
+        csr.style.top = dets.y - 3 + "px"
+
+
+
+    }, 90) // delay in milliseconds (100ms)
+
+})
+
+
+
+
+
+
+
+gsap.to("#nav", {
+    backgroundColor: "#0e0e0e9e",
+    backdropFilter: "blur(10px)",
+    duration: 1,
+    delay:0.5,
+    height: "65.27px",
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        start: "top -15%",
+        end: "top -16%",
+        scrub: 1
+    }
+})
