b804817903fc40b8d838edea939de8b5aec338f1
RK <khatrimrehan@gmail.com>
2026-03-28 00:41:10 +0530

    haha

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-
-
-
-
-var csr = document.querySelector("#cursor")
-
-
-document.addEventListener("mousemove", function (dets) {
-
-    setTimeout(function () {
-
-        csr.style.left = dets.x + 0 + "px"
-        csr.style.top = dets.y - 3 + "px"
-
-
-
-    }, 90) // delay in milliseconds (100ms)
-
-})
+// create the scrollSmoother before your scrollTriggers
+ScrollSmoother.create({
+	smooth: 0.5, // how long (in seconds) it takes to "catch up" to the native scroll position
+	effects: true, // looks for data-speed and data-lag attributes on elements
+	smoothTouch: 0.1 // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
 
 
 
